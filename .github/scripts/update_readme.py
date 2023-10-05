e32ad6a8ccad76c6ccfc676d506e1b8634baa9bb
aloke majumder <aloke.13@gmail.com>
2023-10-05 15:57:26 +0530

    Update update_readme.py
    Again modified to fix the 2nd line issue, 
    
    Signed-off-by: aloke majumder <aloke.13@gmail.com>

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -8,10 +8,16 @@ for filename in os.listdir(profiles_dir):
     if filename.endswith(".md"):
         with open(os.path.join(profiles_dir, filename), 'r') as f:
             lines = f.readlines()
-            name = lines[1].split(":")[1].strip().replace("'", "")
-            image = lines[2].split(":")[1].strip().replace("'", "")
-            bio = lines[3].split(":")[1].strip().replace("'", "")
-            profiles.append((name, image, bio))
+            if len(lines) < 4:  # Check if the file has at least 4 lines
+                print(f"Warning: File {filename} has fewer lines than expected. Skipping...")
+                continue
+            try:
+                name = lines[1].split(":")[1].strip().replace("'", "")
+                image = lines[2].split(":")[1].strip().replace("'", "")
+                bio = lines[3].split(":")[1].strip().replace("'", "")
+                profiles.append((name, image, bio))
+            except IndexError:
+                print(f"Warning: File {filename} doesn't match the expected format. Skipping...")
 
 with open(readme_path, 'r') as f:
     readme_content = f.read()
@@ -28,3 +34,4 @@ new_content = readme_content.split(start_placeholder)[0] + start_placeholder + "
 
 with open(readme_path, 'w') as f:
     f.write(new_content)
+

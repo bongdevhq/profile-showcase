240488cb7f60e0423bbd409e639e0ae342cfce8a
ChiraagBanerjee <jrdg92@gmail.com>
2023-10-06 15:32:13 +0530

    Update update_readme.py
    Automate image resizing and checking contributor's .md file for any problem
    
    Signed-off-by: ChiraagBanerjee <jrdg92@gmail.com>

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 
 profiles_dir = "content/profiles"
 readme_path = "README.md"
@@ -11,13 +12,20 @@ for filename in os.listdir(profiles_dir):
             if len(lines) < 4:  # Check if the file has at least 4 lines
                 print(f"Warning: File {filename} has fewer lines than expected. Skipping...")
                 continue
-            if len(lines[0]) > 9:
+            if len(lines[0]) > 9: #line 0 should not contain any information it's only for markup
                 print(f"Warning: File {filename} doesn't match the expected format. Skipping...")
                 continue
             try:
                 name = lines[1].split(":")[1].strip().replace("'", "")
                 image = lines[2].split(":")[1].strip().replace("'", "")
-                bio = lines[3].split(":")[1].strip().replace("'", "")
+                
+                # Resizing the image to smaller version
+                im = Image.open(f"static/images/{image}")
+                max_size = (250, 250)
+                resized_im = im.resize(max_size)
+                resized_im.save(f"static/images/{image}")
+                
+                bio = lines[4].split(":")[1].strip().replace("'", "")
                 profiles.append((name, image, bio))
             except IndexError:
                 print(f"Warning: File {filename} doesn't match the expected format. Skipping...")
@@ -35,6 +43,5 @@ for name, image, bio in profiles:
 
 new_content = readme_content.split(start_placeholder)[0] + start_placeholder + "\n".join(profile_strings) + end_placeholder
 
-with open(readme_path, 'w', encoding="utf-8") as f:
+with open("readme1.md", 'w', encoding="utf-8") as f:
     f.write(new_content)
-
